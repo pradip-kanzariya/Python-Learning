cc70061bb763121db76fa8a5b4817a0f6ecea07c
pradip-kanzariya <pradip.kanzariya@armakuni.com>
2025-07-17 15:03:51 +0530

    extra practice task 6.1.1

--- task6.py
+++ task6.py
@@ -13,8 +13,39 @@ def random_number_generator():
 print(f"Task-1 : {random_number_generator()}")
 
 
-# Practice Task - 2 | Create a program that lists all files in a directory.
+# Practice Task - 1.1 | Generate a random number between 1 and 100 without using random module and with pure core python logic.
+def generate_random_number():
+    """
+    Generate a random number between 1 and 100 without any module and using core python.
+    """
+    num_list = []
+    second_list = []
+    number = 1.1
+    number_id = id(number)
+    str_number_id = str(number_id)
+    num_list.extend(str_number_id)
 
+    if num_list[-3] == "1" and num_list[-2] == "0" and num_list[-1] == "0":
+        second_list.append(num_list[-3])
+        second_list.append(num_list[-2])
+        second_list.append(num_list[-1])
+    else:
+        if num_list[-2] == "0":
+            second_list.append(num_list[-1])
+        else:
+            second_list.append(num_list[-2])
+            second_list.append(num_list[-1])
+
+    str_join = "".join(second_list)
+    int_num = int(str_join)
+
+    return int_num
+
+
+print(f"Task-1.1 : {generate_random_number()}")
+
+
+# Practice Task - 2 | Create a program that lists all files in a directory.
 def dir_list_files(directory_path: str) -> list:
     """
     Get list of all files names by passing `directory_path`.
